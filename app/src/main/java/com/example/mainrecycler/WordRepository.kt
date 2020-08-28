7105d9c3b49f244aaf004de9912c05d91a930965
PercyKing <57621833+Enobong-kingsley@users.noreply.github.com>
2020-08-28 10:58:13 +0100

    first commit

---
+++ app/src/main/java/com/example/mainrecycler/WordRepository.kt
@@ -0,0 +1,11 @@
+package com.example.mainrecycler
+
+import androidx.lifecycle.LiveData
+
+class WordRepository (private val wordDao: WordDao)  {
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+
+}
